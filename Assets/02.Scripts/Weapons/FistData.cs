d8bff05e9e7497ee790824a751d2d77069abf003
ChanheeKim <cgim971@gmail.com>
2022-07-23 22:10:53 +0900

    no message

--- Assets/02.Scripts/Weapons/FistData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName ="FistData", menuName = "ScriptableObject/Weapons/Fist")]
-public class FistData : ScriptableObject
-{
-    public WeaponInfo _weaponInfo;
-}
